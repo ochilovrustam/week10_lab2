0ad12ace88874f55ad507714e29a015770e035b9
Rustam Ochilov <r.ochilov@newuu.uz>
2025-10-31 12:30:12 +0500

    problem4.cpp

--- problem4.cpp
+++ problem4.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 using namespace std;
     bool isPrime(int n, int i=2) {
-     if (n%i==0)
+    if (n==2)
+        return true;
+    if (n%i==0)
         return false;
-     if (i*i>n)
+    if (i*i>n)
         return true;
     return isPrime(n, i+1);
     }
